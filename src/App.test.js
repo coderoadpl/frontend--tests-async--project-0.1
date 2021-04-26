8688ae91a668facfdaf762e46b2ace393e8c1348
Mateusz Choma <kontakt@coderoad.pl>
2021-04-26 19:21:10 +0200

    users reloading tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,28 @@
 import App from './App'
 
-const MOCK_USER = {
+const MOCK_USER_1 = {
     avatar: 'https://randomuser.me/api/portraits/thumb/men/36.jpg',
     name: 'Mateusz Choma'
 }
 
-const MOCK_USERS = (new Array(10)).fill(MOCK_USER)
+const MOCK_USER_2 = {
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/36.jpg',
+    name: 'Code Road'
+}
+
+const MOCK_USERS_1 = (new Array(10)).fill(MOCK_USER_1)
+const MOCK_USERS_2 = (new Array(10)).fill(MOCK_USER_2)
 
 const loadUsersMocked = async function loadUsers() {
     this.setLoading(true)
     let users = null
     try {
-        users = await Promise.resolve(MOCK_USERS)
+        users = await Promise.resolve(
+            this.users === null ?
+                MOCK_USERS_1
+                :
+                MOCK_USERS_2
+        )
         this.setUsers(users)
     } catch (error) {
         console.error('Error loading users', error)
@@ -58,9 +69,10 @@ describe('state after first load', () => {
         app1.init()
 
         queueMicrotask(() => {
-            expect(app1.users).toStrictEqual(MOCK_USERS)
+            expect(app1.users).toStrictEqual(MOCK_USERS_1)
             done()
         })
+
     })
 
     it('should have users loaded - jest mock function results', async () => {
@@ -71,8 +83,35 @@ describe('state after first load', () => {
 
         const users = await app1.loadUsers.mock.results[0].value
 
-        expect(users).toStrictEqual(MOCK_USERS)
-        expect(app1.users).toStrictEqual(MOCK_USERS)
+        expect(users).toStrictEqual(MOCK_USERS_1)
+        expect(app1.users).toStrictEqual(MOCK_USERS_1)
+    })
+
+})
+
+describe('users reloading', () => {
+
+    it('should have users reloaded after 2 seconds', async () => {
+        jest.useFakeTimers('modern')
+
+        expect.assertions(4)
+
+        const app1 = createMockedApp()
+        app1.init()
+
+        const users1 = await app1.loadUsers.mock.results[0].value
+
+        expect(users1).toStrictEqual(MOCK_USERS_1)
+        expect(app1.users).toStrictEqual(MOCK_USERS_1)
+
+        jest.advanceTimersByTime(2000)
+
+        const users2 = await app1.loadUsers.mock.results[1].value
+
+        expect(users2).toStrictEqual(MOCK_USERS_2)
+        expect(app1.users).toStrictEqual(MOCK_USERS_2)
+
+        jest.useRealTimers()
     })
 
 })
