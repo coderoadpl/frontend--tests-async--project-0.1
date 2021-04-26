8c89daa68fa129d30b8320a06a295c8eb1005456
Mateusz Choma <kontakt@coderoad.pl>
2021-04-26 18:44:04 +0200

    `loadUsersMocked` added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
 import App from './App'
 
+const MOCK_USER = {
+    avatar: 'https://randomuser.me/api/portraits/thumb/men/36.jpg',
+    name: 'Mateusz Choma'
+}
+
+const MOCK_USERS = (new Array(10)).fill(MOCK_USER)
+
+const loadUsersMocked = async function loadUsers() {
+    this.setLoading(true)
+    let users = null
+    try {
+        users = await Promise.resolve(MOCK_USERS)
+        this.setUsers(users)
+    } catch (error) {
+        console.error('Error loading users', error)
+    } finally {
+        this.setLoading(false)
+        this.scheduleLoadUsers()
+    }
+    return users
+}
+
+const createMockedApp = () => {
+    const app = new App()
+    app.loadUsers = loadUsersMocked
+    return app
+}
+
 describe('initial state', () => {
 
     it('before init', () => {
-        const app1 = new App()
+        const app1 = createMockedApp()
 
         expect(app1.container).toBe(null)
         expect(app1.users).toBe(null)
@@ -11,7 +39,7 @@ describe('initial state', () => {
     })
 
     it('after init', () => {
-        const app1 = new App()
+        const app1 = createMockedApp()
         app1.init()
 
         expect(app1.container instanceof HTMLDivElement).toBe(true)
